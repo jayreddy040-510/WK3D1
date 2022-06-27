57a1c96d33f79a76dbd917d0d98fce519e151b1a
Jay Reddy <jayreddy@gmail.com>
2022-06-27 16:04:43 -0700

    finished up until my_zip

--- Enumerables_Project/enumerables.rb
+++ Enumerables_Project/enumerables.rb
@@ -112,11 +112,36 @@ end
 # my_flatten should return all elements of the array into a new, one-dimensional array. Hint: use recursion!
 
 class Array
-    def my_flatten
+    def my_flatten(data = self)
         return [data] if !data.is_a?(Array)
-
+        data.inject([]) {|acc, cur| acc + self.my_flatten(cur)}
 #         self.my_flatten
     end
 end
 
-p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
+# p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
+
+
+# My Zip
+# Write my_zip to take any number of arguments. It should return a new array containing self.length elements. Each element of the new array should be an array with a length of the input arguments + 1 and contain the merged elements at that index. If the size of any argument is less than self, nil is returned for that location.
+class Array
+    def my_zip(*arrays)
+
+        self.map.with_index {|ele, i| arrays.inject(ele) { |acc, cur| acc + cur[i]}}    
+        
+    end
+end
+
+
+
+
+
+a = [ 4, 5, 6 ]
+b = [ 7, 8, 9 ]
+[1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
+[1, 2].my_zip(a, b)    # => [[1, 4, 7], [2, 5, 8]]
+
+c = [10, 11, 12]
+d = [13, 14, 15]
+[1, 2].my_zip(a, b, c, d)    # => [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
